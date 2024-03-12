50b37a4d30f088b159e79af1522b353576f1a738
aryan <aryankap@usc.edu>
2024-03-12 11:59:12 -0700

    removed server side code to handle on client side, for netlify deploy

--- src/App.js
+++ src/App.js
@@ -8,22 +8,23 @@ function App() {
   const [token, setToken] = useState('');
 
   useEffect(() => {
+    const hash = window.location.hash
+      .substring(1)
+      .split('&')
+      .reduce((initial, item) => {
+        if (item) {
+          const parts = item.split('=');
+          initial[parts[0]] = decodeURIComponent(parts[1]);
+        }
+        return initial;
+      }, {});
 
-    async function getToken() {
-      const response = await fetch('/auth/token');
-      const json = await response.json();
-      setToken(json.access_token);
+    if (hash.access_token) {
+      setToken(hash.access_token);
     }
-
-    getToken();
-
   }, []);
 
-  return (
-    <>
-        { (token === '') ? <Login/> : <WebPlayback token={token} /> }
-    </>
-  );
+  return <>{token ? <WebPlayback token={token} /> : <Login />}</>;
 }
 
 
--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
+const clientId = '51c41fdf0b4e45c5b99dbb0795aabbf3';
+const redirectUri = 'http://localhost:3000/callback';
+const scopes = 'streaming user-read-email user-read-private user-read-currently-playing playlist-read-private playlist-read-collaborative user-read-playback-state user-modify-playback-state';
+
+const LOGIN_URL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}`;
+
 function Login() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <a className="btn-spotify" href="/auth/login" >
-                    Login with Spotify 
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="btn-spotify" href={LOGIN_URL}>
+          Login with Spotify
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default Login;
-
